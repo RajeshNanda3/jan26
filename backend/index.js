5834f65a6a193878f3523724dc590852b88e48c2
RajeshNanda3 <153210406+RajeshNanda3@users.noreply.github.com>
2026-03-08 18:35:13 +0530

    for vercel index

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,7 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-// export default app;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+export default app;
